a0f7ee4b4504af7caa7f81426775e81ce9e75f73
Ben Drucker <bvdrucker@gmail.com>
2014-04-22 18:35:46 -0400

    lint + test

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('lint', function () {
+  return gulp.src(['src/*.js', 'test/*.js', 'gulpfile.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.jshint.reporter('fail'));
+});
+
+gulp.task('test', function (cb) {
+  require('test-setup');
+  gulp.src('src/*.js')
+    .pipe(plugins.istanbul())
+    .on('end', function () {
+      gulp.src('test/*.js')
+        .pipe(plugins.mocha())
+        .pipe(plugins.istanbul.writeReports())
+        .on('end', cb);
+    });
+});
